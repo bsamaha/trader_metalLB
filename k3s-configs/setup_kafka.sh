d7bf62da10573c564713bb7feb400d760b658930
Blake S <blake.samaha16@gmail.com>
2024-07-26 13:16:02 -0500

    using helm chart instead of deployment

--- k3s-configs/setup_kafka.sh
+++ k3s-configs/setup_kafka.sh
@@ -8,6 +8,8 @@ fi
 
 # Add Bitnami repo (for Kafka chart)
 helm repo add bitnami https://charts.bitnami.com/bitnami
+# Add Kafdrop repo
+helm repo add kafdrop https://obsidiandynamics.github.io/kafdrop
 helm repo update
 
 # Install Kafka
@@ -21,15 +23,24 @@ kubectl wait --namespace kafka \
              --selector=app.kubernetes.io/name=kafka \
              --timeout=300s
 
-# Create a Secret for Kafdrop with Kafka password
-echo "Creating Secret for Kafdrop..."
+# Get Kafka password
 KAFKA_PASSWORD=$(kubectl get secret kafka-user-passwords --namespace kafka -o jsonpath='{.data.client-passwords}' | base64 -d | cut -d , -f 1)
-kubectl create secret generic kafdrop-kafka-secret --from-literal=kafka-password=$KAFKA_PASSWORD -n kafka
 
-# Install Kafdrop
+# Install Kafdrop using Helm
 echo "Installing Kafdrop..."
-kubectl apply -f k3s-configs/kafka/kafdrop-deployment.yaml
-kubectl apply -f k3s-configs/kafka/kafdrop-service.yaml
+helm upgrade -i kafdrop kafdrop/kafdrop \
+    --namespace kafka \
+    --set kafka.brokerConnect="kafka-broker-0.kafka-broker-headless.kafka.svc.cluster.local:9092,kafka-broker-1.kafka-broker-headless.kafka.svc.cluster.local:9092,kafka-broker-2.kafka-broker-headless.kafka.svc.cluster.local:9092" \
+    --set kafka.properties="security.protocol=SASL_PLAINTEXT\nsasl.mechanism=PLAIN\nsasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username=\"user1\" password=\"$KAFKA_PASSWORD\";" \
+    --set server.servlet.contextPath="/" \
+    --set cmdArgs="--message.format=DEFAULT --topic.deleteEnabled=false --topic.createEnabled=false" \
+    --set jvm.opts="-Xms32M -Xmx64M" \
+    --set service.type=LoadBalancer \
+    --set service.port=80 \
+    --set resources.requests.cpu=100m \
+    --set resources.requests.memory=128Mi \
+    --set resources.limits.cpu=500m \
+    --set resources.limits.memory=512Mi
 
 echo "Kafka and Kafdrop setup complete!"
 echo "Kafdrop will be accessible via LoadBalancer. Run 'kubectl get svc -n kafka' to find the external IP."
